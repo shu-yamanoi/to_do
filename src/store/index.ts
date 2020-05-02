5f4abeebf519f759961f91c4d07cd4015e98e9dd
shu-yamanoi <surayama1111@gmail.com>
2020-05-02 13:40:37 +0900

    型定義ファイル作成

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,106 +1,56 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { Todo } from './types'
 
 Vue.use(Vuex)
-
-
-type Todo = {
-  title: string;
-  id: number;
-  priority: 'HIGH' | 'MEDIUM' | 'LOW';
-  status: 'TODO' | 'DOING' | 'DONE';
-}
-
 
 export default new Vuex.Store({
   state: {
     todos: [] as Todo[],
-    doings: [] as Todo[],
-    dones: [] as Todo[],
-    idState: 0 as number
+    idState: 0
   },
 
   mutations: {
-    create(state, newTodo) {
-      const todo = {
+    addTodo(state, newTodo) {
+      const todo: Todo = {
         title: '',
         id: 0,
-        priority: 'LOW' as 'HIGH' | 'MEDIUM' | 'LOW',
-        status: 'TODO' as 'TODO'
+        priority: 'LOW',
+        status: 'TODO'
       }
       todo.title = newTodo.title
       todo.priority = newTodo.priority
-      todo.id = state.idState
+      todo.id = state.idState++
       state.todos.push(todo)
-      state.idState++
     },
     
 
-    add(state, todo) {
-      if(todo.status === 'TODO') {
-        for(let i = 0; i < state.todos.length; i++) {
-          if(state.todos[i].id === todo.id) {
-            const todo = {
-              title: state.todos[i].title,
-              id: state.todos[i].id,
-              status: 'DOING' as 'DOING',
-              priority: state.todos[i].priority as 'HIGH' | 'MEDIUM' | 'LOW'
-            }
-            state.doings.push(todo)
-            state.todos.splice(i, 1)
-            break
-          }
-        }
-      }else if(todo.status === 'DOING') {
-        for(let i = 0; i < state.doings.length; i++) {
-          if(state.doings[i].id === todo.id) {
-            const todo = {
-              title: state.doings[i].title,
-              id: state.doings[i].id,
-              status: 'DONE' as 'DONE',
-              priority: state.doings[i].priority as 'HIGH' | 'MEDIUM' | 'LOW'
-            }
-            state.dones.push(todo)
-            state.doings.splice(i, 1)
-            break
-          }
+    changeStatus(state, todo) {
+      for(let i = 0; i < state.todos.length; i++) {
+        if(state.todos[i].id === todo.id) {
+          state.todos[i].status = todo.status === 'TODO' 
+            ? 'DOING'
+            : 'DONE'
+          state.todos.splice(i, 1, state.todos[i])
+          break
         }
       }
     },
 
     delete(state, todo) {
-      if(todo.status === 'TODO') {
-        for(let i = 0; i < state.todos.length; i++) {
-          if(state.todos[i].id === todo.id) {
-            state.todos.splice(i, 1)
-            break
-          }
-        }
-      }else if(todo.status === 'DOING') {
-        for(let i = 0; i < state.doings.length; i++) {
-          if(state.doings[i].id === todo.id) {
-            state.doings.splice(i, 1)
-            break
-          }
-        }
-      }else {
-        for(let i = 0; i < state.dones.length; i++) {
-          if(state.dones[i].id === todo.id) {
-            state.dones.splice(i, 1)
-            break
-          }
-        }
-      }
+      const newTodos: Todo[] = state.todos.filter(_todo => _todo.id !== todo.id)
+      state.todos.splice(0)
+      Array.prototype.push.apply(state.todos, newTodos)
     }
   },
 
   actions: {
-    create({ commit }, newTodo) {
-      commit('create', newTodo)
+    add({ commit }, newTodo) {
+      commit('addTodo', newTodo)
     },
 
     ToNextState({ commit }, todo) {
-      commit('add', todo)
+      commit('changeStatus', todo)
     },
 
     DeleteTodo({ commit }, todo) {
